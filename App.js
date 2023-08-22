14cb6b4c0bdec563e2cb28103589e7d2cedc0984
nlijin <nlijinraj@gmail.com>
2023-08-22 12:08:11 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "this is the new h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "this is the new h2 tag"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+// crossorigin;
